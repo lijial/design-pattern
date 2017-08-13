ec52416f7c49568c8f4f029890f8033e86c739db
lijialiang <jialiang.li@tongdun.cn>
2017-08-13 10:32:41 +0800

    适配器模式的主文件

--- src/main/java/adapter/pattern/AdapterPatternMain.java
+++ src/main/java/adapter/pattern/AdapterPatternMain.java
@@ -4,6 +4,8 @@ package adapter.pattern;
  * 适配器模式（Adapter Pattern)
  * 意图：将一个类的接口转换成客户希望的另外一个接口。适配器模式使得原本由于接口不兼容而不能一起工作的那些类可以一起工作。
  * http://www.runoob.com/design-pattern/adapter-pattern.html
+ * 心得：如果已有工具想多支持功能，可以写个适配器，适配器里可以new 出高级工具来支持mp4格式，而
+        原来内置功能只支持mp3，这样可以不改原来的结构
  * Created by lijialiang on 2017/8/11.
  *
  */
